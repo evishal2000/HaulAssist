4e9c3cf4f887a69a1f947b1ae2e266f028ed3923
Amogh Krishna Padakanti <amoghkrishna16@gmail.com>
2025-02-28 21:16:57 -0500

    added google maps api to get co-ordinates based on place name

---
+++ backend/cmd/api/map_handler.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"haulassist_backend/internal/service"
+	"net/http"
+)
+
+// GetCoordinatesHandler handles the request to get coordinates of a place
+func GetCoordinatesHandler(w http.ResponseWriter, r *http.Request) {
+	place := r.URL.Query().Get("place")
+	if place == "" {
+		http.Error(w, "Place is required", http.StatusBadRequest)
+		return
+	}
+
+	lat, lng, err := service.GetCoordinates(place)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]float64{"latitude": lat, "longitude": lng}
+	json.NewEncoder(w).Encode(response)
+}
