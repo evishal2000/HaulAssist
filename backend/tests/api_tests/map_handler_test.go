23e9f48531f8658d047c0181ac684a724e479309
Amogh Krishna Padakanti <amoghkrishna16@gmail.com>
2025-03-03 20:30:47 -0500

    map_handler unit tests

---
+++ backend/tests/api_tests/map_handler_test.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"haulassist_backend/cmd/api"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+// MockService is a mock implementation of the service package
+
+func TestGetCoordinatesHandler(t *testing.T) {
+
+	godotenv.Load("../../.env")
+
+	t.Run("Place is required", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/coordinates", nil)
+		w := httptest.NewRecorder()
+
+		api.GetCoordinatesHandler(w, req)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("Successful response", func(t *testing.T) {
+
+		req := httptest.NewRequest("GET", "/coordinates?place=New+York", nil)
+		w := httptest.NewRecorder()
+
+		api.GetCoordinatesHandler(w, req)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		var respBody map[string]float64
+		json.NewDecoder(resp.Body).Decode(&respBody)
+
+		assert.Equal(t, 40.7127753, respBody["latitude"])
+		assert.Equal(t, -74.0059728, respBody["longitude"])
+	})
+
+	t.Run("Error from service", func(t *testing.T) {
+
+		req := httptest.NewRequest("GET", "/coordinates?place=Unknown+Place", nil)
+		w := httptest.NewRecorder()
+
+		api.GetCoordinatesHandler(w, req)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+	})
+}
